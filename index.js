fbb68c699fe5acca4fc11b616e011ffa47b24c97
Momen <touristmomen@gmail.com>
2024-10-11 22:49:37 +0600

    vercel added, cookie parsar, jwt added, token and admin middleware added, gadgets collection added

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://gadxtreme-906da.web.app",
+      "https://gadxtreme.netlify.app",
+    ],
     credentials: true,
   })
 );
 app.use(express.json());
+app.use(cookieParser());
 
 const client = new MongoClient(process.env.URI, {
   serverApi: {
@@ -21,12 +28,76 @@ const client = new MongoClient(process.env.URI, {
   },
 });
 
+const isToken = async (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      res.status(401).send({ message: "unauthorized access" });
+    } else {
+      req.decodedUser = decoded;
+      next();
+    }
+  });
+};
+
 async function run() {
   try {
     // await client.connect();
+    const gadgetsCollection = client.db("gadXtreme").collection("gadgets");
 
+    // const isAdmin = async (req, res, next) => {
+    //   const email = req.decodedUser.email;
+    //   const user = await userCollection.findOne({ email });
 
-    
+    //   if (email === demoAdmin) {
+    //     // Restrict the demo admin to read-only access
+    //     req.demoAdmin = true;
+    //     return next();
+    //   }
+    //   if (!user || user?.role !== "admin") {
+    //     return res.status(403).send({ message: "forbidden access" });
+    //   }
+    //   next();
+    // };
+
+    app.post("/jwt", async (req, res) => {
+      try {
+        const userEmail = req?.body;
+        // console.log("user for token", userEmail);
+        const getToken = jwt.sign(userEmail, process.env.ACCESS_TOKEN, {
+          expiresIn: "5d",
+        });
+        res
+          .cookie("token", getToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          })
+          .send({ success: true });
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    app.get("/logout", async (req, res) => {
+      try {
+        // const user = req.body;
+        // console.log(user);
+        res
+          .clearCookie("token", {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          })
+          .send({ success: true });
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. Successfully connected to MongoDB!");
   } finally {
@@ -36,7 +107,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("GadXtreme Running Server Successfully");
+  res.send("Welcome to GadXtreme e-commerce Server");
 });
 
 app.listen(port, () => {
